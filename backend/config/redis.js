a11e417b62e962019aacb09b56297f4b139ab16e
rahulthapa9024 <rahulthapa9024@gmail.com>
2025-10-09 00:37:41 +0530

    fix

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -2,7 +2,7 @@ const { createClient }  = require('redis');
 
 const redisClient = createClient({
     username: 'default',
-    password: "D92t1m3vGqMYdP9WA0yFwf02FLQR1StH",
+    password: process.env.REDIS_PASS ,
     socket: {
         host: 'redis-16968.c258.us-east-1-4.ec2.redns.redis-cloud.com',
         port: 16968
